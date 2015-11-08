8160e789f349782acc8ed3bd115a293f3b8c51e0
haoxin <coderhaoxin@outlook.com>
2015-11-08 21:14:50 +0800

    test: coverage++

--- test/index.js
+++ test/index.js
@@ -1,43 +1,66 @@
+
 'use strict';
 
-let assert = require('assert');
-let pg = require('..');
+const assert = require('assert');
+const pg = require('../');
 
 const config = 'postgres://hx@localhost/hx';
 
-describe('## pg-then', function() {
-  describe('# Pool', function() {
-    let pool;
+describe('## pg-then', () => {
+  describe('# Pool', () => {
 
-    it('new pool', function(done) {
-      pool = pg.Pool(config);
-
-      setTimeout(done, 1000);
+    it('invalid db uri', (done) => {
+      pg.Pool('postgres://hx@localhost:3333/hx')
+        .connect()
+        .catch((err) => {
+          assert.ok(err.code, 'ECONNREFUSED');
+          done();
+        });
     });
 
-    it('query', function() {
-      return pool.query('SELECT 1 AS count').then(function(result) {
-        assert.equal(result.rowCount, 1);
-        assert.equal(result.rows[0].count, 1);
-      });
+    it('invalid query', (done) => {
+      pg.Pool(config)
+        .query('invalid sql')
+        .catch((err) => {
+          assert.ok(err.message.startsWith('syntax error'));
+          done();
+        });
+    });
+
+    it('query', () => {
+      return pg.Pool(config)
+        .query('SELECT 1 AS count')
+        .then((result) => {
+          assert.equal(result.rowCount, 1);
+          assert.equal(result.rows[0].count, 1);
+        });
     });
   });
 
-  describe('# Client', function() {
+  describe('# Client', () => {
     let client;
 
-    it('new client', function(done) {
+    it('new client', (done) => {
       client = pg.Client(config);
 
       setTimeout(done, 1000);
     });
 
-    it('query', function() {
-      return client.query('SELECT 1 AS count').then(function(result) {
-        assert.equal(result.rowCount, 1);
-        assert.equal(result.rows[0].count, 1);
-        client.end();
-      });
+    it('invalid query', (done) => {
+      client.query('invalid sql')
+        .catch((err) => {
+          assert.ok(err.message.startsWith('syntax error'));
+          done();
+        });
+    });
+
+    it('query', () => {
+      return client.query('SELECT 1 AS count')
+        .then((result) => {
+          assert.equal(result.rowCount, 1);
+          assert.equal(result.rows[0].count, 1);
+          client.end();
+        });
     });
   });
 });
